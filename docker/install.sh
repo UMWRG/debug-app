d477acafeea0030bcfb1589c55cad2dd9631c5b0
Stephen Knox <knoxp@gmail.com>
2019-04-09 11:18:00 +0100

    Update the dockerfile to ubuntu instead of alpine

--- docker/install.sh
+++ docker/install.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 pip install pipenv
 # Install all the dependencies 
-pipenv install --system --deploy 
+pipenv install --system --deploy --verbose 
 
 # Clean up the cache
 rm -rf ~/.cache/pip
