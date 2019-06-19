7fcae6e03d218bdb9a85d5ad10368a1fe170aa9f
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-06-19 16:21:45 +0100

    new commands

--- hydra_debug/cli.py
+++ hydra_debug/cli.py
@@ -1,11 +1,6 @@
-import click 
+import click
 from hydra_debug import exporter, importer, run
-
-def hydra_app(category='import'):
-    def hydra_app_decorator(func):
-        func.hydra_app_category = category
-        return func
-    return hydra_app_decorator
+from hydra_client.click import hydra_app, make_plugins, write_plugins
 
 @click.group()
 @click.pass_obj
@@ -25,7 +20,7 @@ def start_cli():
     cli(obj={}, auto_envvar_prefix='HYDRA_DEBUG')
 
 @hydra_app(category='export')
-@cli.command(name='export')
+@cli.command(name='debug-app-export')
 @click.pass_obj
 @click.option('-n',  '--network-id', help='Network ID')
 @click.option('-s',  '--scenario-id', help='Scenario ID')
@@ -36,11 +31,11 @@ def start_cli():
 @click.option('--fail', default=False)
 @click.option('--data-dir', default='/tmp')
 def do_export(obj, network_id, scenario_id, dummy1, dummy2, dummy3, switch_on, fail, data_dir):
-    
+
     exporter.do_export(network_id, scenario_id, dummy1, dummy2, dummy3, switch_on, fail, data_dir)
 
 @hydra_app(category='import')
-@cli.command(name='import')
+@cli.command(name='debug-app-import')
 @click.pass_obj
 @click.option('-p',  '--project-id', help='Project ID')
 @click.option('-f', '--data-file', help="Data File")
@@ -51,26 +46,38 @@ def do_export(obj, network_id, scenario_id, dummy1, dummy2, dummy3, switch_on, f
 @click.option('--fail', default=False)
 @click.option('--data-dir', default='/tmp')
 def do_import(obj, project_id, data_file, dummy1, dummy2, dummy3, switch_on, fail, data_dir):
-    
+
     importer.do_import(project_id, data_file, dummy1, dummy2, dummy3, switch_on, fail, data_dir)
 
 @hydra_app(category='model')
-@cli.command(name='run')
+@cli.command(name='debug-app-run')
 @click.pass_obj
 @click.option('-n',  '--network-id', help='Network ID')
 @click.option('-t', '--timeout', default=5, help='How log to sleep for')
 @click.option('--fail', 'fail', flag_value=True)
-def do_run(obj, network_id, timeout, fail):
+@click.option('--heavy-load', 'heavy_load', flag_value=True)
+def do_run(obj, network_id, timeout, fail, heavy_load):
 
-    run.do_run(network_id, timeout, fail)
+    run.do_run(network_id, timeout, fail, heavy_load)
+
+# @cli.command()
+# @click.pass_obj
+# @click.option('--all', is_flag=True, help='By default only the Export, Run, Import is registered. This flag registers the import, export and auto apps')
+# def register(obj, all=False):
+#
+#     run.register()
+#
+#     if all is True:
+#         importer.register()
+#         exporter.register()
+
+
 
 @cli.command()
 @click.pass_obj
-@click.option('--all', is_flag=True, help='By default only the Export, Run, Import is registered. This flag registers the import, export and auto apps')
-def register(obj, all=False):
-
-    run.register()
-
-    if all is True:
-        importer.register()
-        exporter.register()
+@click.argument('docker-image', type=str)
+def register(obj, docker_image):
+    """ Register the app with the Hydra installation. """
+    plugins = make_plugins(cli, 'hydra-debug', docker_image=docker_image)
+    app_name = docker_image.replace('/', '-').replace(':', '-')
+    write_plugins(plugins, app_name)
